81f20fd03db71d1b3917c1d4991ab055e52fbd01
Thekiborg <none>
2024-12-16 23:29:03 +0100

    Broke atlas

---
+++ 1.5/Source/DMTRDefOf.cs
@@ -0,0 +1,16 @@
+﻿namespace DynamicMealTextureReplacer
+{
+#pragma warning disable CA2211
+	[DefOf]
+	public static class DMTRDefOf
+	{
+		public static ThingDef DMTR_FillWithAllMeatDefs;
+		public static ThingDef DMTR_FillWithAllInsectMeatDefs;
+		public static ThingDef DMTR_FillWithAllEggDefs;
+
+		static DMTRDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(DMTRDefOf));
+		}
+	}
+}
